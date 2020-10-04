98b4cd5c930dda0fe27c21fb8f538c21e8efb239
Hayri Zelhar <5DEYYH32@solent.ac.uk>
2020-10-04 18:23:43 +0000

    Added code to display an ascii art robot.

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,16 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+
+
+print("     ___     ")
+print("    |_ _|    ")
+print("   ((O O))   ")
+print('   _|\_/|_   ')
+print('  / \___/ \  ')
+print(' /  /   \  \ ')
+print('   |     |   ')
+print('   |     |   ')
